eef6c3948ddaa348193128ef493d726e3be319a2
IvanTulin <tulin.v@bk.ru>
2024-08-20 13:43:26 +0300

    create branch develop

---
+++ ChatMessanger/ChatMessanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatMessanger
+//
+//  Created by Ivan Tulin on 20.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
